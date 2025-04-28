f6c17316fc63e6801fe7a5836b0e2a0b523e307b
Alerik Muller <344325@via.dk>
2025-04-28 14:39:20 +0200

    possible fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
