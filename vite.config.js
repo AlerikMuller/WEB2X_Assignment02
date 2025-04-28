e6338f4bbbae1021f51ad87f898efaf7a752e31a
Alerik Muller <344325@via.dk>
2025-04-28 14:45:40 +0200

    posiible fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/pokedex-react/',
   plugins: [react()],
-  base: '/WEB2X_Assignment02/',
 }) // finished
